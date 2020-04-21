fc9f0de363eb7fe531b33b7145e31b58ceac994b
thecuong <thecuongkma240397@gmail.com>
2020-04-21 17:18:49 +0700

    Add package Chalk

--- Dog.js
+++ Dog.js
@@ -1,6 +1,14 @@
-function Dog(){
+ var chalk = require ('chalk'); 
+function Dog(name){
     this.stomach = [] ;
+    this.name = name ; 
 }
 Dog.prototype.eat = function (Cat){
     this.stomach.push(Cat) ; 
 }
+
+Dog.prototype.sayHi = function () { 
+    console.log('Hi, i"m '+ chalk.blue(this.name));
+}
+
+module.exports = Dog ;
